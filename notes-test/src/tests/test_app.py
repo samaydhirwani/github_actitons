a7819e3e2ae76ae1b9d9c23e8e6635dd89f47347
samaydhirwani <samay.dhirwani@gmail.com>
2024-05-23 14:41:50 +0530

    Update test_app.py

--- notes-test/src/tests/test_app.py
+++ notes-test/src/tests/test_app.py
@@ -7,7 +7,7 @@ class TestApp(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(1, 2), 3)
         self.assertEqual(add(-1, 1), 0)
-        self.assertEqual(add(-1, -1), -2)
+        self.assertEqual(add(0, 0), 0)
 
     def test_subtract(self):
         self.assertEqual(subtract(2, 1), 1)
